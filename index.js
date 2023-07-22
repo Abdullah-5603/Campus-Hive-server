67461cdb117db85f0a8e66f361cb92c2bfc28ea3
Abu Abdullah <abdullah72308@gmail.com>
2023-07-22 21:51:05 +0600

    candidates and curent-user api added

--- index.js
+++ index.js
@@ -31,19 +31,60 @@ const dbConnect = async () => {
 dbConnect()
 
 const usersCollection = client.db('CampusHive').collection('users')
+const collegeCollection = client.db('CampusHive').collection('colleges')
+const candidateCollection = client.db('CampusHive').collection('candidates')
 
 // users api
 
-app.post('/users', async(req, res) => {
-    const user = req.body
+app.post('/all-users', async (req, res) => {
+    const user = req.body;
+    const query = { email: user?.email }
+    const existingUser = await usersCollection.findOne(query);
+    if (existingUser) {
+        return res.send({})
+    }
     const result = await usersCollection.insertOne(user)
     res.send(result)
+})
+
+app.get('/all-users', async (req, res) => {
+    const result = await usersCollection.find().toArray()
+    res.send(result)
+})
+
+app.get('/current-user', async (req, res) => {
+    const email = req.query.email;
+    const query = { email: email }
+    const result = await usersCollection.findOne(query)
+    res.send(result)
+})
+
+// candidates api
+
+app.post('/candidates', async (req, res) => {
+    const candidate = req.body;
+    const result = await candidateCollection.insertOne(candidate)
+    res.send(result)
+})
+
+// college api
+
+app.get('/all-colleges', async (req, res) => {
+    const result = await collegeCollection.find().toArray()
+    res.send(result)
+})
+
+app.get('/all-colleges/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)}
+    const result = await collegeCollection.findOne(query)
+    res.send(result);
 })
 
 app.get('/', (req, res) => {
     res.send('Campus Hive is Running')
 })
 
-app.listen(port, (req, res) =>{
+app.listen(port, (req, res) => {
 
 })
